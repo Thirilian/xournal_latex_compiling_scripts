feb32a4e801614b4601c9d878f22743cda3c6dcc
Thirilian <125454675+Thirilian@users.noreply.github.com>
2026-04-24 22:51:19 +0200

    Compiling past errors
    creates multiple versions of the input and compile past errrors
    
    Improvements : 
    - recovered the on-the-fly compilation
    Regressions : 
    - Lost the LaTeX error message
    - going past errors is a bad idea

--- latex_script.sh
+++ latex_script.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 TEX_FILE="$1"
+TEMP_VALID_TEX="$(mktemp --suffix=.tex)"
 
+# Essayer de compiler le fichier LaTeX d'origine
 pdflatex -interaction=nonstopmode "$TEX_FILE"
+
 if [ $? -ne 0 ]; then
-    FALLBACK_TEX="$(mktemp --suffix=.tex)"
-    cat > "$FALLBACK_TEX" <<EOF
-\documentclass{standalone}
-\usepackage{xcolor}
-\begin{document}
-{\Huge\color{red} Invalid LaTeX formula}
-\end{document}
-EOF
-    pdflatex -interaction=nonstopmode -jobname=tex "$FALLBACK_TEX"
-    rm "$FALLBACK_TEX"
+    # Générer un document contenant la dernière version valide
+    echo "Dernière version valide de la formule :" > "$TEMP_VALID_TEX"
+    cat "$TEX_FILE" >> "$TEMP_VALID_TEX"
+
+    # Ajouter le message d'erreur
+    echo "\\\\[1em]" >> "$TEMP_VALID_TEX"  # Ajoute un petit espace vertical
+    echo "{\\Huge\\color{red} Erreur}" >> "$TEMP_VALID_TEX"
+
+    # Compiler la dernière version valide
+    pdflatex -interaction=nonstopmode -jobname=last_valid "$TEMP_VALID_TEX"
+
+    # Nettoyer
+    rm "$TEMP_VALID_TEX"
+else
+    # Si la compilation réussit, sauvegarder le fichier
+    mv "$TEX_FILE" "${TEX_FILE%.tex}_valid.tex"
 fi
