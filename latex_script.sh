b8ba52227a8f5778db9668b2d9bcd26d535e7e8f
Thirilian <125454675+Thirilian@users.noreply.github.com>
2026-04-24 23:19:30 +0200

    Adding a %%% parameter [NOT WORKING]
    - Adding the possibility to write "%%%" in the formula to go in "errorful mode", wich would compile the formula as xournal would
    
    Improvements
    - option to get latex outputs in case of errors

--- latex_script.sh
+++ latex_script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
 TEX_FILE="$1"
 TEMP_VALID_TEX="$(mktemp --suffix=.tex)"
 
 # Try to compile the original file
 pdflatex -interaction=nonstopmode "$TEX_FILE"
 
-if [ $? -ne 0 ]; then # If the compilation contained errors
+if grep -q '%%%' "$TEX_FILE"; then #If %%% was written to go to error-display mode
+    konsole --noclose -e bash -c "echo 'pute' && less -R '$TEMP_VALID_TEX'"
+    pdflatex -interaction=nonstopmode "$TEMP_VALID_TEX"
+elif [ $? -ne 0 ]; then # If the compilation contained errors
 
     # Generate a temporary tex file
     echo "Dernière version valide de la formule :" > "$TEMP_VALID_TEX"
@@ -18,12 +20,11 @@ if [ $? -ne 0 ]; then # If the compilation contained errors
     ' "$TEX_FILE"
     # Recompile the original (errorless) file to show an error message
     pdflatex -interaction=nonstopmode "$TEX_FILE"
-        #konsole --noclose -e bash -c "less -R '$TEMP_VALID_TEX'"
+        konsole --noclose -e bash -c "echo 'ducon' && less -R '$TEX_FILE'"
 
     # Cleanup of the temporary file
     rm "$TEMP_VALID_TEX"
 else
-
     # If the original compilation was successful
     mv "$TEX_FILE" "${TEX_FILE%.tex}_valid.tex"
 fi
