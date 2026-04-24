c90bd4a7f937fc90024599eeabd9ff584dca3302
Thirilian <125454675+Thirilian@users.noreply.github.com>
2026-04-24 22:58:49 +0200

    Add error handling for LaTeX compilation failures
    This version appends " \LaTeX Error" to a formula in a box that can't be compiled successfuly. The displayed formula then corresponds to the last successful version, followed with a written message
    
    Improvements : 
    - allows on-the-fly compilation AND an error message
    Disadvantages : the code assumes formula is written to line 29 (assumes using the default template)

--- latex_script.sh
+++ latex_script.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
+
 TEX_FILE="$1"
 TEMP_VALID_TEX="$(mktemp --suffix=.tex)"
 
-# Essayer de compiler le fichier LaTeX d'origine
+# Try to compile the original file
 pdflatex -interaction=nonstopmode "$TEX_FILE"
 
-if [ $? -ne 0 ]; then
-    # Générer un document contenant la dernière version valide
+if [ $? -ne 0 ]; then # If the compilation contained errors
+
+    # Generate a temporary tex file
     echo "Dernière version valide de la formule :" > "$TEMP_VALID_TEX"
     cat "$TEX_FILE" >> "$TEMP_VALID_TEX"
 
-    # Ajouter le message d'erreur
-    echo "\\\\[1em]" >> "$TEMP_VALID_TEX"  # Ajoute un petit espace vertical
-    echo "{\\Huge\\color{red} Erreur}" >> "$TEMP_VALID_TEX"
+    # modify the original file to add the error message
+    sed -i '29a\
+    \\rouge{\\text{ Erreur \\LaTeX }}
+    ' "$TEX_FILE"
+    # Recompile the original (errorless) file to show an error message
+    pdflatex -interaction=nonstopmode "$TEX_FILE"
+        #konsole --noclose -e bash -c "less -R '$TEMP_VALID_TEX'"
 
-    # Compiler la dernière version valide
-    pdflatex -interaction=nonstopmode -jobname=last_valid "$TEMP_VALID_TEX"
-
-    # Nettoyer
+    # Cleanup of the temporary file
     rm "$TEMP_VALID_TEX"
 else
-    # Si la compilation réussit, sauvegarder le fichier
+
+    # If the original compilation was successful
     mv "$TEX_FILE" "${TEX_FILE%.tex}_valid.tex"
 fi
