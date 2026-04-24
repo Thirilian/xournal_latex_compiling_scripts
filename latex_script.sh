2a8f263d7dde1b69b7c2f000183c100899d083c6
Thirilian <125454675+Thirilian@users.noreply.github.com>
2026-04-24 23:33:52 +0200

    First working version - options Error marking, errorful and fast-compile
    # This is a working version that follows version 3.
    # This script tries  to compile the  LaTeX code. If the code contains errors, the last displayable version will be
    # displayed, followed with a red sign "\LaTeX Errors"
    
    # In case you need it, you can write '%%%' at the end of your code, in wich case the LaTeX formula won't be compiled and
    # error infos will be outputed in xournal
    
    # Another possibility is to write '%F' at the end of your code to invoke a command that allows to compile past-errors
    # (some signs will be missing, no errors will be outputed and the formula will be compiled) and the real-time
    # compilation will be much faster

--- latex_script.sh
+++ latex_script.sh
@@ -1,6 +1,3 @@
-# This version is not working.
-# The most advanced version is wrritten to xournal_latex_wrapper-working
-
 #!/bin/bash
 TEX_FILE="$1"
 TEMP_VALID_TEX="$(mktemp --suffix=.tex)"
@@ -9,21 +6,35 @@ TEMP_VALID_TEX="$(mktemp --suffix=.tex)"
 pdflatex -interaction=nonstopmode "$TEX_FILE"
 pdflatex_status=$?
 
-if grep -Fq '%%%' "$TEX_FILE"; then
-    pdflatex -interaction=nonstopmode "$TEMP_VALID_TEX"
-elif grep -Fq '%F' "$TEX_FILE" && [ "$pdflatex_status" -ne 0 ]; then
-    echo "Dernière version valide de la formule :" > "$TEMP_VALID_TEX"
-    cat "$TEX_FILE" >> "$TEMP_VALID_TEX"
-    pdflatex -interaction=nonstopmode -jobname=last_valid "$TEMP_VALID_TEX"
-    rm "$TEMP_VALID_TEX"
-elif [ "$pdflatex_status" -ne 0 ]; then
-    echo "Dernière version valide de la formule :" > "$TEMP_VALID_TEX"
-    cat "$TEX_FILE" >> "$TEMP_VALID_TEX"
-    sed -i '29a\
-    \\rouge{\\text{ Erreur \\LaTeX }}
-    ' "$TEX_FILE"
+if grep -Fq '%%%' "$TEX_FILE"; then #If %%% was written to go to error-display mode
     pdflatex -interaction=nonstopmode "$TEX_FILE"
+
+elif grep -Fq '%F' "$TEX_FILE" && [ "$pdflatex_status" -ne 0 ]; then #If %F was written go to fast compile mode
+    echo "Last valid version of the formula :" > "$TEMP_VALID_TEX"
+    cat "$TEX_FILE" >> "$TEMP_VALID_TEX"
+
+    # Compile the temporary file ignoring errors
+    pdflatex -interaction=nonstopmode -jobname=last_valid "$TEMP_VALID_TEX"
+
+    # Clean up the temporary file
     rm "$TEMP_VALID_TEX"
+
+elif [ "$pdflatex_status" -ne 0 ]; then # If the compilation contained errors and no option was input
+    echo "Last valid version of the formula :" > "$TEMP_VALID_TEX"
+    cat "$TEX_FILE" >> "$TEMP_VALID_TEX"
+
+    # modify the original (compiled) file to add the error message
+    #See the note and modify accordingly
+    sed -i '29a\
+    {\\color{red} \\text{ Erreur \\LaTeX }}
+    ' "$TEX_FILE"
+
+    # Recompile the original (errorless) file to show an error message
+    pdflatex -interaction=nonstopmode "$TEX_FILE"
+    # Clean up the temporary file
+    rm "$TEMP_VALID_TEX"
+
 else
+    # The formula doesn't contain errors, save it
     mv "$TEX_FILE" "${TEX_FILE%.tex}_valid.tex"
 fi
